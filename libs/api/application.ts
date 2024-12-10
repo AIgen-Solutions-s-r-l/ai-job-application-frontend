6bbf022c423ec89beb2a4db7a69d07a205dec17f
Arystanbek Jetisý <cholicer@gmail.com>
2024-12-10 18:51:49 +0500

    arys: sumbits application to application manager

---
+++ libs/api/application.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { MatchingJob } from "../definitions";
+import { apiClientJwt } from "./client";
+import API_BASE_URLS from "./config";
+
+export async function createJobApplication(jobs: MatchingJob[]): Promise<{ success: boolean; error?: string }> {
+  try {
+    const data = {
+      "jobs": jobs,
+    }
+
+    const response = await apiClientJwt.post(`${API_BASE_URLS.application}/applications`, data, {
+      headers: {
+        Accept: "application/json",
+      },
+    })
+
+    if (response.status !== 200) {
+      return {
+        success: false,
+        error: `Server returned ${response.status}: ${response.data?.error || response.statusText}`,
+      };
+    }
+
+    if (!response.data) {
+      return { success: false, error: "Application failed" };
+    }
+
+    return { success: true};
+  } catch (error) {
+    console.error(`Error creating job application: ${jobs}`, error);
+    return { success: false, error: error.message };
+  }
+}
--- libs/api/config.ts
+++ libs/api/config.ts
@@ -1,6 +1,7 @@
 const API_BASE_URLS = {
     resumes: process.env.RESUMES_API_URL,
     matching: process.env.MATCHING_API_URL,
+    application: process.env.APPLICATION_API_URL,
     auth: process.env.AUTH_API_URL,
   };
   

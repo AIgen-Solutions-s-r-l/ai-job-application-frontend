869b704e05ddf2679befc47bdb4ee6aa81b119f7
Arystanbek Jetisý <cholicer@gmail.com>
2025-01-09 17:44:32 +0500

    wip: Testing with cv template

--- libs/data.ts
+++ libs/data.ts
@@ -1,10 +1,11 @@
 import { createClient } from "@/libs/supabase/server";
-import { CVType, JobProfile, MatchingJob, JobSearchParams, AppliedJob, DetailedPendingApplication, PendingApplicationRecord } from "./definitions";
+import { CVType, JobProfile, MatchingJob, JobSearchParams, AppliedJob, PendingApplicationRecord } from "./definitions";
 import { fetchUserResume } from "@/libs/api/resume";
 import { toJobProfile } from "./job-profile-util";
 import { fetchMatchingJobs } from "./api/matching";
 import { fetchAppliedJobs } from "./api/application";
 import { fetchDetailedApplicationData, fetchPendingApplications } from "./api/apply_pending";
+import { DetailedPendingApplication } from "./types/application.types";
 
 export async function getCVAction(): Promise<CVType> {
   const supabase = createClient();

376cfa68349209e6ee9c3d2d0460b8af5b1ccd32
Arystanbek Jetisý <cholicer@gmail.com>
2025-01-12 15:58:35 +0500

    Added something

--- components/job-application/ResumeSections/ResumeAdditional.tsx
+++ components/job-application/ResumeSections/ResumeAdditional.tsx
@@ -1,7 +1,9 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Resume } from '../trash/application.types';
-import { useFormContext } from 'react-hook-form';
+import { useFieldArray, useFormContext } from 'react-hook-form';
 import Link from 'next/link';
+import { NullifiedInput } from '@/components/ui/nullified-input';
+import { Minus, Plus } from 'lucide-react';
 
 type FormData = Pick<Resume, "additionalInfo">
 
@@ -39,19 +41,220 @@ const SkillsNestedFieldArray: React.FC = () => {
         placeholder="e.g., React, TypeScript, Node.js, Git (separate skills with commas)"
         className="grow resize-none overflow-y-hidden border-b-2 border-transparent outline-none bg-transparent focus:outline-secondary placeholder-shown:border-black"
       />
-      
+    </div>
+  ) : null;
+}
+
+const LanguageNestedFieldArray: React.FC = (): React.ReactElement => {
+  const { getValues, setValue } = useFormContext<FormData>();
+  const languages = getValues('additionalInfo.languages');
+  const refinedArray = languages.map((lang) => lang.language);
+
+  const debounce = (func: (...args: any[]) => void, delay: number) => {
+    let timeout: ReturnType<typeof setTimeout> | undefined; // Correct type
+    return (...args: any[]) => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => func(...args), delay);
+    };
+  };
+
+  const handleLanguagesChange = useCallback(
+    (value: string) => {
+      const languagesArray = value.split(',').map(language => language.trim()).filter(Boolean);
+      const refined = languagesArray.map((lang) => (
+        {
+         "language": lang,
+         "proficiency": 'native',
+        }
+      ));
+      setValue(`additionalInfo.languages`, refined);
+    },
+    [setValue]
+  );
+
+  const debouncedHandleLanguagesChange = useCallback(
+    debounce(handleLanguagesChange, 500), 
+    [handleLanguagesChange] 
+  );
+
+  return refinedArray.length ? (
+    <div className="flex flex-wrap text-xs leading-none mt-2 gap-1">
+      <span className="font-semibold">Languages: </span>
+      <textarea
+        onChange={(e) => debouncedHandleLanguagesChange(e.target.value)} // Use the debounced function
+        defaultValue={refinedArray ? refinedArray.join(', ') : ''}
+        placeholder="Language"
+        rows={1}
+        className="grow resize-none overflow-y-hidden border-b-2 border-transparent outline-none bg-transparent focus:outline-secondary placeholder-shown:border-black"
+      />
+    </div>
+  ) : null;
+}
+
+const ProjectsNestedFieldArray: React.FC = (): React.ReactElement => {
+  const { register, getValues } = useFormContext<FormData>();
+  const { fields, append, remove } = useFieldArray({ 
+    name: `additionalInfo.side_projects`
+  })
+  const side_projects = getValues('additionalInfo.side_projects');
+
+  const handleAddProject = () =>
+    append({
+      name: "",
+      description: "",
+      link: "",
+    }
+  );
+
+  return fields.length ? (
+    <div className="flex flex-wrap text-xs mt-2 gap-1 leading-none">
+      <span className="font-semibold">Projects: </span>
+      <ul className="list-disc list-inside ml-4 w-full relative">
+        {fields.map((exp, index) => (
+          <li className="pb-1 flex items-start relative" key={exp.id}>
+            <div className="absolute top-0 -left-6 w-8 h-8 items-center gap-10 group">
+              {<button 
+                className={fields.length === 1 ? 'hidden' : 'hidden group-hover:flex'}
+                onClick={() => remove(index)}
+              >
+                <Minus className='font-bold text-base-content' size={16} strokeWidth={4}  />
+              </button>}
+            </div>
+            <Link href={side_projects[index].link} target="_blank" rel="noopener noreferrer" className="font-normal text-blue-500 relative">
+              <NullifiedInput
+                {...register(`additionalInfo.side_projects.${index}.name`)}
+                placeholder="Project Name"
+              />
+            </Link>
+            &nbsp;&#8209;&nbsp;
+            <textarea
+              {...register(`additionalInfo.side_projects.${index}.description`)}
+              placeholder="Project Description"
+              className="grow resize-none overflow-y-hidden border-b-2 border-transparent outline-none bg-transparent focus:outline-secondary placeholder-shown:border-black"
+            />
+          </li>
+          )
+        )}
+        <div className="absolute -bottom-6 -left-6 w-8 h-8 items-center gap-10 group">
+          {<button 
+            className='hidden group-hover:flex'
+            onClick={handleAddProject}
+          >
+            <Plus className='font-bold text-base-content' size={16} strokeWidth={3}  />
+          </button>}
+        </div>
+      </ul>
+    </div>
+  ) : null;
+}
+
+const AchievementsNestedFieldArray: React.FC = (): React.ReactElement => {
+  const { register } = useFormContext<FormData>();
+  const { fields, append, remove } = useFieldArray({ 
+    name: `additionalInfo.achievements`
+  })
+
+  const handleAddAchievement = () =>
+    append({
+      name: "",
+      description: "",
+    }
+  );
+
+  return fields.length ? (
+    <div className="flex flex-wrap text-xs mt-2 gap-1 leading-none">
+      <span className="font-semibold">Achievements: </span>
+      <ul className="list-disc list-inside ml-4 w-full relative">
+        {fields.map((exp, index) => (
+          <li className="pb-1 flex items-start relative" key={exp.id}>
+            <div className="absolute top-0 -left-6 w-8 h-8 items-center gap-10 group">
+              {<button 
+                className={fields.length === 1 ? 'hidden' : 'hidden group-hover:flex'}
+                onClick={() => remove(index)}
+              >
+                <Minus className='font-bold text-base-content' size={16} strokeWidth={4}  />
+              </button>}
+            </div>
+            <NullifiedInput
+              {...register(`additionalInfo.achievements.${index}.name`)}
+              placeholder="Achievement Name"
+            />
+            :&nbsp;
+            <textarea
+              {...register(`additionalInfo.achievements.${index}.description`)}
+              placeholder="Achievement Description"
+              className="grow resize-none overflow-y-hidden border-b-2 border-transparent outline-none bg-transparent focus:outline-secondary placeholder-shown:border-black"
+            />
+          </li>
+          )
+        )}
+        <div className="absolute -bottom-6 -left-6 w-8 h-8 items-center gap-10 group">
+          {<button 
+            className='hidden group-hover:flex'
+            onClick={handleAddAchievement}
+          >
+            <Plus className='font-bold text-base-content' size={16} strokeWidth={3}  />
+          </button>}
+        </div>
+      </ul>
+    </div>
+  ) : null;
+}
+
+const CertificationsNestedFieldArray: React.FC = (): React.ReactElement => {
+  const { register } = useFormContext<FormData>();
+  const { fields, append, remove } = useFieldArray({ 
+    name: `additionalInfo.certifications`
+  })
+  
+  const handleAddCertification = () =>
+    append({
+      name: "",
+      description: "",
+    }
+  );
+
+  return fields.length ? (
+    <div className="flex flex-wrap text-xs mt-2 gap-1 leading-none">
+      <span className="font-semibold">Certifications: </span>
+      <ul className="list-disc list-inside ml-4 w-full relative">
+        {fields.map((exp, index) => (
+          <li className="pb-1 flex items-start relative" key={exp.id}>
+            <div className="absolute top-0 -left-6 w-8 h-8 items-center gap-10 group">
+              {<button 
+                className={fields.length === 1 ? 'hidden' : 'hidden group-hover:flex'}
+                onClick={() => remove(index)}
+              >
+                <Minus className='font-bold text-base-content' size={16} strokeWidth={4}  />
+              </button>}
+            </div>
+            <NullifiedInput
+              {...register(`additionalInfo.certifications.${index}.name`)}
+              placeholder="Certification Name"
+            />
+            :&nbsp;
+            <textarea
+              {...register(`additionalInfo.certifications.${index}.description`)}
+              placeholder="Certification Description"
+              className="grow resize-none overflow-y-hidden border-b-2 border-transparent outline-none bg-transparent focus:outline-secondary placeholder-shown:border-black"
+            />
+          </li>
+          )
+        )}
+        <div className="absolute -bottom-6 -left-6 w-8 h-8 items-center gap-10 group">
+          {<button 
+            className='hidden group-hover:flex'
+            onClick={handleAddCertification}
+          >
+            <Plus className='font-bold text-base-content' size={16} strokeWidth={3}  />
+          </button>}
+        </div>
+      </ul>
     </div>
   ) : null;
 }
 
 export const ResumeAdditional: React.FC = () => {
-  const { register, getValues } = useFormContext<FormData>();
-  const additional_skills = getValues('additionalInfo.additional_skills');
-  const languages = getValues('additionalInfo.languages');
-  const side_projects = getValues('additionalInfo.side_projects');
-  const achievements = getValues('additionalInfo.achievements');
-  const certifications = getValues('additionalInfo.certifications');
-
   return (
     <div className="mt-8" id="skills-section text-xs">
       <h1 className="text-xs font-semibold tracking-wide w-full border-b-4 border-black pb-2 uppercase">
@@ -59,35 +262,15 @@ export const ResumeAdditional: React.FC = () => {
       </h1>
       
       <SkillsNestedFieldArray />
+
+      <LanguageNestedFieldArray />
+
+      <ProjectsNestedFieldArray />
       
-      {languages.length && (
-        <div className="flex flex-wrap text-xs mt-2 gap-1 leading-none">
-          <span className="font-semibold">Languages: </span>
-          {languages.map((exp, index) => {
-            return (
-              <p key={index} className="inline">{exp.language} ({exp.proficiency}){index !== languages.length - 1 ? ', ' : '.'}</p>
-            )
-          })}
-        </div>
-      )}
-      {side_projects.length && (
-        <div className="flex flex-wrap text-xs mt-2 gap-1 leading-none">
-          <span className="font-semibold">Projects: </span>
-          <ul className="list-disc list-inside ml-4">
-            {side_projects.map((exp, index) => {
-              return (
-                <li className="pb-1" key={index}>
-                  <Link href={exp.link} target="_blank" rel="noopener noreferrer" className="font-normal text-blue-500">
-                    {exp.name} -
-                  </Link>
-                  <p className="inline ml-1">{exp.description}</p>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
-      )}
-      {achievements.length && (
+      <AchievementsNestedFieldArray />
+
+      <CertificationsNestedFieldArray />      
+      {/* {achievements.length && (
         <div className="flex flex-wrap text-xs mt-2 gap-1 leading-none">
           <span className="font-semibold">Achievements: </span>
           <ul className="list-disc list-inside ml-4">
@@ -120,7 +303,7 @@ export const ResumeAdditional: React.FC = () => {
             })}
           </ul>
         </div>
-      )}
+      )} */}
     </div>
   );
 };

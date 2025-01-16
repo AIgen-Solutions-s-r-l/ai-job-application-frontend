fcd3710df2c0f40b10a8142a496b56bb2e902ad1
Arystanbek Jetisý <cholicer@gmail.com>
2025-01-16 16:38:09 +0500

    wip: doc template

--- components/job-application/ResumeSections/ResumePersonal.tsx
+++ components/job-application/ResumeSections/ResumePersonal.tsx
@@ -1,71 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Resume } from '../trash/application.types';
 import { useFormContext } from 'react-hook-form';
 import { NullifiedInput } from '@/components/ui/nullified-input';
 import { Mail, MapPin, Phone } from 'lucide-react';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { useClickOutside } from '@/libs/hooks/useClickOutside';
+import { cn } from '@/lib/utils';
 
 export const ResumePersonal: React.FC = () => {
   const { register, getValues } = useFormContext<Resume>();
   const linkedin: string = getValues('personalInfo.linkedin');
   const github: string = getValues('personalInfo.github');
+  const { ref, isActive, setIsActive } = useClickOutside();
   
-  return (
-    <div className="flex flex-col gap-4" id="personal_section">
-      <div className="flex flex-col items-center">
-        <h1 className="font-semibold tracking-widest text-xl">
+  return <>
+    {isActive && <div className="fixed top-0 left-0 bottom-0 right-0 bg-black/50 z-30" />}
+    <div 
+      className={cn('flex flex-col items-center border-2 border-transparent has-[:focus]:border-secondary bg-white relative', isActive && 'border-secondary z-30')}
+      id="personal_section"
+      ref={ref}
+      onClick={() => setIsActive(true)}
+    >
+      <h1 className="font-semibold tracking-widest text-xl">
+        <NullifiedInput
+          {...register('personalInfo.name')}
+          placeholder="Name"
+        />
+        &nbsp;
+        <NullifiedInput
+          {...register('personalInfo.surname')}
+          placeholder="Surname"
+        />
+      </h1>
+      <ul className="mt-3 text-gray-700 text-xs flex flex-row gap-2 list-none">
+        <li className="flex gap-[2px]">
+          <MapPin size={16} />
           <NullifiedInput
-            {...register('personalInfo.name')}
-            placeholder="Name"
+            {...register('personalInfo.country')}
+            placeholder="Country"
           />
-          &nbsp;
+          ,&nbsp;
           <NullifiedInput
-            {...register('personalInfo.surname')}
-            placeholder="Surname"
+            {...register('personalInfo.city')}
+            placeholder="City"
           />
-        </h1>
-        <ul className="mt-3 text-gray-700 text-xs flex flex-row gap-2 list-none">
-          <li className="flex gap-[2px]">
-            <MapPin size={16} />
-            <NullifiedInput
-              {...register('personalInfo.country')}
-              placeholder="Country"
-            />
-            ,&nbsp;
-            <NullifiedInput
-              {...register('personalInfo.city')}
-              placeholder="City"
-            />
-          </li>
-          <li className="flex gap-[2px]">
-            <Phone size={16} />
-            <NullifiedInput
-              {...register('personalInfo.phone_prefix')}
-              placeholder="+1"
-              className="min-w-1"
-            />
-            <NullifiedInput
-              {...register('personalInfo.phone')}
-              placeholder="1234567890"
-            />
-          </li>
-          <li className="flex gap-[2px]">
-            <Mail size={16} />
-            <NullifiedInput
-              {...register('personalInfo.email')}
-              placeholder="Email"
-            />
-          </li>
-          <li className="flex gap-[2px]">
-            <FaLinkedin size={16} />
-            <a href={linkedin} className="text-blue-500" target="_blank">Linkedin</a>
-          </li>
-          <li className="flex gap-[2px]">
-            <FaGithub size={16} />
-            <a href={github} className="text-blue-500" target="_blank">Github</a>
-          </li>
-        </ul>
-      </div>
+        </li>
+        <li className="flex gap-[2px]">
+          <Phone size={16} />
+          <NullifiedInput
+            {...register('personalInfo.phone_prefix')}
+            placeholder="+1"
+            className="min-w-1"
+          />
+          <NullifiedInput
+            {...register('personalInfo.phone')}
+            placeholder="1234567890"
+          />
+        </li>
+        <li className="flex gap-[2px]">
+          <Mail size={16} />
+          <NullifiedInput
+            {...register('personalInfo.email')}
+            placeholder="Email"
+          />
+        </li>
+        <li className="flex gap-[2px]">
+          <FaLinkedin size={16} />
+          <a href={linkedin} className="text-blue-500" target="_blank">Linkedin</a>
+        </li>
+        <li className="flex gap-[2px]">
+          <FaGithub size={16} />
+          <a href={github} className="text-blue-500" target="_blank">Github</a>
+        </li>
+      </ul>
     </div>
-  );
+  </>;
 };

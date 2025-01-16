d44bac15164aef72e114f784aead786f1287c263
Arystanbek Jetisý <cholicer@gmail.com>
2025-01-16 20:20:38 +0500

    wip: testing with context

--- components/job-application/ResumeSections/ResumeEducation.tsx
+++ components/job-application/ResumeSections/ResumeEducation.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Resume } from '../trash/application.types';
 import { useFieldArray, useFormContext } from 'react-hook-form';
 import { NullifiedInput } from '@/components/ui/nullified-input';

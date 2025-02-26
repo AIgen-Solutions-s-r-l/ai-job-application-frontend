74c236b290b53c2088764dc69b4947d3f75c740c
Arystanbek Jetisý <cholicer@gmail.com>
2025-02-26 13:48:20 +0500

    job searching works fine

--- components/job-search/JobSearchView.tsx
+++ components/job-search/JobSearchView.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import { JobSearchProps, MatchingJob } from '@/libs/definitions';
-import { useRouter } from 'next/navigation';
 import { JobSearchBar } from './JobSearchBar';
 import { JobFeedList } from './JobFeedList';
 import { JobSearchBottomSheet } from './JobSearchBottomSheet';
@@ -17,22 +16,10 @@ export const JobSearchView: React.FC<JobSearchViewProps> = ({
   initialJobs,
   searchParams,
 }) => {
-  const router = useRouter();
-
-  const onSearch = (searchProps: JobSearchProps) => {
-    const params = new URLSearchParams();
-
-    for (const [key, value] of Object.entries(searchProps)) {
-      params.set(key, value);
-    }
-
-    router.push(`?${params.toString()}`);
-  };
-
   return (
     <JobSearchProvider initialJobs={initialJobs}>
       <div className='w-full flex flex-col items-center'>
-        <JobSearchBar onSearch={onSearch} searchParams={searchParams} />
+        <JobSearchBar searchParams={searchParams} />
         <JobFeedList />
         <JobSearchBottomSheet />
       </div>

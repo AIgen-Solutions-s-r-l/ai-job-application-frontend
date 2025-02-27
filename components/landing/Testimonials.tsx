7e369cfc2931bc42a49394eca7ba1f2d762566ba
sanhijazi <h.hijazi711@gmail.com>
2025-02-27 18:52:30 +0100

    Fix card width

--- components/landing/Testimonials.tsx
+++ components/landing/Testimonials.tsx
@@ -5,8 +5,8 @@ import Heart from './Heart';
 export const Testimonials: React.FC = () => {
   return (
     <section className='bg-primary-light-purple pt-[100px] md:pt-[140px] 2xl:pt-[166px]'>
-      <LandingContainer>
-        <div className='flex items-center justify-between mb-[150px] gap-[10px]'>
+      <LandingContainer className='xl:px-[10vw] 2xl:px-[10vw]'>
+        <div className='flex items-center justify-between mb-[70px] gap-[10px]'>
           <Heart />
           <h2 className="text-3xl md:text-[35px] 2xl:text-[40px] font-medium leading-[1.1] text-white">Don’t take our word for it!<br />Hear it from our customers</h2>
           <Heart />

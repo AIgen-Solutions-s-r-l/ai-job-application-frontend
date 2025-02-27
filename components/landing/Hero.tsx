4eb1f8a87e16648be36763163a65d48acba7094c
sanhijazi <h.hijazi711@gmail.com>
2025-02-27 18:31:40 +0100

    add

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -16,10 +16,10 @@ export const Hero: React.FC = () => {
   };
 
   return (
-    <LandingContainer className='px-[20vw]'>
+    <LandingContainer className=' mb:px-[20vw]'>
       <section className='flex flex-col items-center gap-[20px] md:gap-[25px] 2xl:gap-[35px] font-montserrat pt-[50px] 2xl:pt-[45px]'>
         <h1 className="text-heading">Auto-apply fast with AI.<br /> Get your dream job while sleeping</h1>
-        <div className="relative w-[68%] max-w-[1204px]">
+        <div className="relative w-[100%] md:w-[68%] max-w-[1204px]">
           <Image src="/landing/hero-screen.png" alt="hero-screen" width={1204} height={1042} className='w-full h-auto max-w-[1204px] z-2' />
           <div className="absolute top-[5%] left-1/2 transform -translate-x-1/2 w-[90%] max-w-[1078px] z-1">
             {!isPlaying && (

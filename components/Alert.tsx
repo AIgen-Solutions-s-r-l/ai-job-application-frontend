8ff049189d9b7012b263091e8b3cc39fb331575b
max <mcnic@users.noreply.github.com>
2025-03-06 22:42:42 -0800

    feat: imlementing new design to Job Application History Page

---
+++ components/Alert.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { FC, ReactNode } from 'react';
+import Image from 'next/image';
+import CloseButton from '@/components/svgs/CloseBtn.svg';
+
+type Props = {
+  children?: ReactNode;
+  onClose?: () => void;
+};
+
+export const Alert: FC<Props> = ({ children, onClose }) => (
+  <div className='relative bg-primary-light-purple-gray rounded-xl p-5'>
+    <button className='outline-none absolute top-4 right-4' onClick={onClose}>
+      <Image src={CloseButton} width={14} height={14} alt='close button' />
+    </button>
+    {children}
+  </div>
+);
--- components/jobs/JobFeed.tsx
+++ components/jobs/JobFeed.tsx
@@ -11,25 +11,10 @@ export default async function JobFeed() {
   ]);
 
   return (
-    <div className='font-light flex flex-col gap-5 rounded-2xl font-jura'>
-      <h1 className='text-3xl font-semibold'>Job Application History</h1>
-
-      {/* todo (new disign in figma), is need redo? */}
-      <div className='flex flex-col gap-5'>
-        <div className='text-2xl'>
-          <b>Congratulations!</b> &nbsp;
-          <span>
-            We sent your application to{' '}
-            <b>{Object.keys(appliedJobs).length} jobs.</b>
-          </span>
-        </div>
-      </div>
-
-      <JobFeedList
-        appliedJobs={appliedJobs}
-        failedJobs={failedJobs}
-        isLoading={false}
-      />
-    </div>
+    <JobFeedList
+      appliedJobs={appliedJobs}
+      failedJobs={failedJobs}
+      isLoading={false}
+    />
   );
 }

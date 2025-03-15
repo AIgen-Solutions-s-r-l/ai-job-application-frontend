04187574b98bfb7546006e1859560dc1e05bd9d7
max <mcnic@users.noreply.github.com>
2025-03-15 15:20:26 -0700

    fixL according VerifyEmail form like all auth forms

--- components/VerifyEmailInstruction.tsx
+++ components/VerifyEmailInstruction.tsx
@@ -1,17 +1,25 @@
 'use client';
 
 import Link from 'next/link';
+import Image from 'next/image';
+import config from '@/config';
 
 export const VerifyEmailInstruction = () => (
-  <div className='h-screen flex justify-center items-center'>
-    <div className='border border-my-neutral-3 rounded-lg p-5 flex flex-col gap-4'>
-      <h2>A email has been sent to you!</h2>
-      <p>To complete registration, follow the link we sent you in an email.</p>
-      <span>
+  <main className='auth-form-main' data-theme={config.colors.theme}>
+    <div className='auth-form-container'>
+      <div className='flex flex-col gap-2'>
+        <Image src='/laboro.png' alt='Logo' width={214} height={58} />
+        <h2 className='auth-form-header'>A email has been sent to you!</h2>
+        <p>
+          To complete registration, follow the link we sent you in an email.
+        </p>
+      </div>
+
+      <div className='auth-form-footer'>
         <Link href='/' className='text-primary'>
           to main page
         </Link>
-      </span>
+      </div>
     </div>
-  </div>
+  </main>
 );

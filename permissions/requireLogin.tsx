84f339800600ec27bfe49b828e9340793f0dea8b
sanhijazi <h.hijazi711@gmail.com>
2025-03-17 22:32:22 +0200

    Fix redirection

--- permissions/requireLogin.tsx
+++ permissions/requireLogin.tsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState, useCallback } from 'react';
 import { useRouter } from "next/navigation";
 import { getServerCookie } from "@/libs/cookies";
 import config from '@/config';
+import { useUserContext } from '@/contexts/user-context';
 
 type ComponentType<Props = {}> = React.ComponentType<Props>;
 
 const RequireLogin = <P extends object>(Component: ComponentType<P>, Resume: boolean = true): React.FC<P> => {
     const LoginComponent: React.FC<P> = (props) => {
-        // const { user } = useUserContext();
+        const { user } = useUserContext();
         const router = useRouter();
         const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
@@ -20,14 +21,14 @@ const RequireLogin = <P extends object>(Component: ComponentType<P>, Resume: boo
             else {
                 setIsAuthenticated(true);
             }
-            // if (user) {
-            //     if (user.exists && !Resume) {
-            //         router.replace("/dashboard");
-            //     } else if (!user.exists && Resume) {
-            //         router.replace("/onboarding");
-            //     }
-            // }
-        }, [router]);
+            if (user) {
+                if (user.exists && !Resume) {
+                    router.replace("/dashboard");
+                } else if (!user.exists && Resume) {
+                    router.replace("/onboarding");
+                }
+            }
+        }, [router, user]);
 
         useEffect(() => {
             checkAuthentication();

33f36d5417a9017a7183a30faa4ae8cc4e938a5b
sanhijazi <h.hijazi711@gmail.com>
2025-04-06 17:05:22 +0200

    Fix Footer

--- components/landing/Footer.tsx
+++ components/landing/Footer.tsx
@@ -8,8 +8,8 @@ export const Footer: FC = () => {
   return (
     <footer className='bg-my-neutral-7 pt-[37px] pb-[45px] xl:pt-[47px] xl:pb-[53px]'>
       <LandingContainer className='xl:px-[10vw] md:px-[10vw] px-[5vw]'>
-        <div className="w-full grid grid-cols-2 md:grid-cols-3 xl:grid-cols-11 justify-center text-center">
-          <div className="col-span-1 md:col-span-1 xl:col-span-3 py-[12px] xl:py-[36px] px-[32px] xl:px-[60px] flex flex-col gap-[40px] xl:border-r border-primary-light-purple items-center">
+        <div className="w-full grid grid-cols-1 md:grid-cols-3 xl:grid-cols-3 justify-center text-center">
+          <div className="py-[12px] xl:py-[36px] px-[32px] xl:px-[60px] flex flex-col gap-[40px] xl:border-r border-primary-light-purple items-center">
             <Link href='/' className='footer-link' >About Us</Link>
             <Link href='/' className='footer-link' >Legal Center</Link>
             <Link href='mailto:info@laboro.co' className='footer-link' >Contact Us</Link>
@@ -22,17 +22,19 @@ export const Footer: FC = () => {
               </Link>
             </div>
           </div>
-          <div className="col-span-2 xl:col-span-4 py-[12px] px-[12px] md:py-[24px] xl:px-[62px] xl:border-r border-primary-light-purple">
+
+          <div className="flex flex-col items-center gap-4 py-[12px] px-[12px] md:py-[24px] xl:px-[62px] xl:border-r border-primary-light-purple order-first md:order-none">
             <Link
               href="/"
             >
               <Image src="/landing/logo.png" alt="Logo" width={428} height={110} className='w-[200px] md:w-[250px] xl:w-[400px] h-auto' />
             </Link>
-            <p className="text-[16px] md:text-[20px] xl:text-[24px] font-medium text-white font-montserrat md:ml-[10px] xl:ml-[24px] mt-[12px] xl:mt-[22px]">AI-automated Job Applications</p>
+            <p className="text-[16px] md:text-[20px] xl:text-[24px] font-medium text-white font-montserrat">AI-automated Job Applications</p>
           </div>
-          <div className="col-span-1 md:col-span-1 xl:col-span-4 py-[36px] px-[12px] xl:px-[60px] flex flex-col gap-[32px] ">
-              <p className="text-[8px] md:text-[12px] xl:text-[16px] font-small text-white font-montserrat md:ml-[10px] xl:ml-[24px] mt-[12px] xl:mt-[22px]">Via Guglielmo Marconi, 45, 40122 Bologna BO</p>
-              <p className="text-[8px] md:text-[12px] xl:text-[16px] font-small text-white font-montserrat md:ml-[10px] xl:ml-[24px] mt-[12px] xl:mt-[22px]">P.IVA: 04284191204</p>
+
+          <div className=" py-[36px] px-[12px] xl:px-[60px] flex flex-col gap-[32px] ">
+            <p className="text-[8px] md:text-[12px] xl:text-[16px] font-small text-white font-montserrat md:ml-[10px] xl:ml-[24px] mt-[12px] xl:mt-[22px]">Via Guglielmo Marconi, 45, 40122 Bologna BO</p>
+            <p className="text-[8px] md:text-[12px] xl:text-[16px] font-small text-white font-montserrat md:ml-[10px] xl:ml-[24px] mt-[12px] xl:mt-[22px]">P.IVA: 04284191204</p>
           </div>
 
         </div>

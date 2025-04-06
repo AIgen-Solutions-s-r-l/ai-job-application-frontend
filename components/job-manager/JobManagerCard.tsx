7cfaf0bca23957e190f076959a39d43d4a872d6a
sanhijazi <h.hijazi711@gmail.com>
2025-04-06 17:42:05 +0200

    Fix card title padding in card job manager

--- components/job-manager/JobManagerCard.tsx
+++ components/job-manager/JobManagerCard.tsx
@@ -32,7 +32,7 @@ export const JobManagerCard: FC<Props> = ({ id, className, job, onClick }) => {
       className={cn("flex flex-col justify-between bg-white rounded-xl relative p-5 gap-1 lg:gap-3 leading-none cursor-pointer", className)}
       onClick={onClick}
     >
-      <h3 className="font-montserrat text-base md:text-xl font-medium">{job.title}</h3>
+      <h3 className="font-montserrat text-base md:text-xl font-medium pr-12">{job.title}</h3>
       <div className='flex items-center justify-between'>
         <h3 className="font-montserrat text-base md:text-xl font-normal">{job.company_name}</h3>
         {job.company_logo && (
@@ -45,13 +45,13 @@ export const JobManagerCard: FC<Props> = ({ id, className, job, onClick }) => {
           </div>
         )}
       </div>
-      {job.location  
-      ? (
-        <p className="text-sm md:text-base flex gap-2 items-center"><Image src={Pin} alt='pin' /> {job.location} | {job.workplace_type}</p>
-      ) 
-      : (
-        <p className="text-sm md:text-base flex gap-2 items-center">{job.workplace_type}</p>
-      )}
+      {job.location
+        ? (
+          <p className="text-sm md:text-base flex gap-2 items-center"><Image src={Pin} alt='pin' /> {job.location} | {job.workplace_type}</p>
+        )
+        : (
+          <p className="text-sm md:text-base flex gap-2 items-center">{job.workplace_type}</p>
+        )}
       <div className='flex gap-x-2 gap-y-1 my-1 lg:my-2 flex-wrap overflow-hidden'>
         {!!job.skills_required.length && job.skills_required.map(
           (skill, index) => (

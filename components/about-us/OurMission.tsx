9e765d1077c1ed2db376ebb33a80aad3018e1568
sanhijazi <h.hijazi711@gmail.com>
2025-04-13 14:45:18 +0200

    About us final

--- components/about-us/OurMission.tsx
+++ components/about-us/OurMission.tsx
@@ -4,6 +4,7 @@ import Line from "@/public/aboutus/line.svg";
 import Start from "@/public/aboutus/Start.svg"
 import Smily from "@/public/landing/laboro-smiley.svg";
 import Star from "./Star";
+import * as motion from "motion/react-client";
 
 export const Circle = ({ invert = false }: { invert?: boolean }) => {
     return (
@@ -14,6 +15,11 @@ export const Circle = ({ invert = false }: { invert?: boolean }) => {
 }
 
 function OurMission() {
+    const positions = {
+        x: ['20%', '91%', '91%', '-2%', '-2%', '5%'],
+        y: ['0%', '0%', '35%', '35%', '70%', '70%']
+    };
+
     return (
         <div className="flex flex-col lg:flex-row min-h-[450px] gap-4 lg:gap-1">
             <div className="flex flex-row lg:flex-col items-center justify-between p-4 lg:py-8 bg-primary-deep-purple w-full lg:w-[205px] rounded-[50px] z-10">
@@ -22,14 +28,29 @@ function OurMission() {
                 <Circle invert={true} />
             </div>
             <div className="relative py-12 flex px-6 lg:pr-[100px] lg:pl-[100px] xl:pl-[200px] gap-4 flex-col items-center justify-center bg-primary-deep-purple w-full rounded-[50px]">
-                <div className="hidden lg:block absolute -left-20 z-0">
+                <div className="hidden lg:block absolute -left-20 z-0 lg:-translate-x-[50px] xl:translate-x-[0px] lg:scale-75 xl:scale-100">
                     <Star />
                 </div>
-                <div className="relative w-full lg:w-auto">
-                    <p className="text-center lg:text-left lg:absolute lg:-top-6 lg:-left-10 font-montserrat text-white text-[24px] lg:text-[28px] font-semibold">Our Mission</p>
-                    <Image src={Line} alt="Line" className="hidden lg:block" />
-                    <Image src={Smily} alt="Smily Face" className="hidden lg:block absolute w-12 -top-5 right-12" />
-                    <p className="text-center lg:text-left mt-4 lg:mt-0 lg:absolute lg:bottom-0 lg:-right-12 xl:-right-0 xxl:-right-4  font-montserrat text-splash-green text-[24px] lg:text-[22px] xxl:text-[28px] font-semibold">Revolutionizing the job application process</p>
+                <div className="relative w-full lg:w-full">
+                    <p className="text-center lg:text-left lg:hidden md:-top-6 lg:-left-10 font-montserrat text-white text-[24px] lg:text-[28px] font-semibold">Our Mission</p>
+                    <Image src={Line} alt="Line" className="hidden lg:block " />
+                    <motion.div
+                        initial={{ left: positions.x[0], top: positions.y[0] }}
+                        animate={{
+                            left: positions.x,
+                            top: positions.y
+                        }}
+                        transition={{
+                            duration: 3,
+                            ease: "easeInOut",
+                            times: [0, 0.17, 0.33, 0.5, 0.67, 0.83, 1]
+                        }}
+                        className="hidden lg:block absolute xl:w-10 lg:w-8  w-12"
+                        style={{ top: '0%', right: '32px' }}
+                    >
+                        <Image src={Smily} alt="Smily Face" />
+                    </motion.div>
+                    <p className="text-center lg:text-left mt-4 lg:mt-0 lg:hidden lg:bottom-0 lg:-right-12 xl:-right-0 xxl:-right-4  font-montserrat text-splash-green text-[24px] lg:text-[22px] xxl:text-[28px] font-semibold">Revolutionizing the job application process</p>
                 </div>
                 <p className="font-montserrat text-white text-[16px] lg:text-[20px] font-[500] text-center lg:text-left mt-4 lg:mt-0">
                     LABORO aims to empower job seekers by streamlining and automating the job application process. Our mission is simple:
@@ -41,4 +62,4 @@ function OurMission() {
     )
 }
 
-export default OurMission
+export default OurMission   
--- components/about-us/WhyLaboro.tsx
+++ components/about-us/WhyLaboro.tsx
@@ -3,6 +3,9 @@ import TheLine from '@/public/aboutus/BlackLine.svg'
 import Person from '@/public/aboutus/Person.svg'
 import CheckMark from '@/public/aboutus/CheckMark.svg'
 import Image from 'next/image';
+import * as motion from "motion/react-client"
+import { useScroll, useTransform, useSpring } from "motion/react"
+import { useRef } from 'react';
 
 
 
@@ -20,19 +23,51 @@ export const List = ({ Header, Text }: { Header: string, Text: String }) => {
     )
 }
 function WhyLaboro() {
+    const sectionRef = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: sectionRef,
+        offset: ["start center", "center end"]
+    });
+
+    const smoothProgress = useSpring(scrollYProgress, {
+        stiffness: 170,
+        damping: 100,
+        restDelta: 0.01
+    });
+
+    const circleX = useTransform(
+        smoothProgress,
+        [0, 0.3, 0.5, 0.6, 1],
+        ['10%', '82%', '82%', '-5%', '-5%']
+    );
+
+    const circleY = useTransform(
+        smoothProgress,
+        [0, 0.3, 0.5, 0.6, 1],
+        ['-3%', '-3%', '29%', '29%', '75%']
+    );
+
     return (
-        <div className='flex flex-col'>
+        <div className='flex flex-col' ref={sectionRef}>
             <div className='flex justify-between px-4 sm:px-8 md:px-12 lg:px-16 xl:px-24'>
                 <p className="font-josefinSans text-black text-[50px] sm:text-[70px] md:text-[120px] lg:text-[160px] xl:text-[200px] font-semibold text-primary-deep-purple leading-[89%] tracking-[-2px] sm:tracking-[-3px] md:tracking-[-4px] lg:tracking-[-5px] xl:tracking-[-6px]">
                     Why<br />Choose<br />Laboro
                 </p>
                 <div className='relative'>
-                    <div className='absolute left-8 -top-4 w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] md:w-[60px] md:h-[60px] lg:w-[70px] lg:h-[70px] xl:w-[80px] xl:h-[80px] bg-splash-orange rounded-full' />
-                    <Image
-                        src={QuestionMark}
-                        alt='Question Mark'
-                        className='w-[60px] sm:w-[80px] md:w-[120px] lg:w-[160px] xl:w-auto'
+                    <motion.div
+                        className='absolute w-[20px] h-[20px] sm:w-[30px] sm:h-[30px] md:w-[40px] md:h-[40px] lg:w-[50px] lg:h-[50px] xl:w-[70px] xl:h-[70px] bg-splash-orange rounded-full'
+                        style={{
+                            left: circleX,
+                            top: circleY,
+                        }}
                     />
+                    <div className='w-[70px] sm:w-[130px] md:w-[180px] lg:w-[225px] xl:w-[300px]'>
+                        <Image
+                            src={QuestionMark}
+                            alt='Question Mark'
+                            className='w-full'
+                        />
+                    </div>
                 </div>
             </div>
             <div>
@@ -44,11 +79,12 @@ function WhyLaboro() {
                     <Image
                         src={Person}
                         alt='Person'
-                        className='absolute -top-[80px] sm:-top-[100px] md:-top-[150px] lg:-top-[200px] xl:-top-[275px] right-4 sm:right-8 md:right-12 lg:right-16 xl:right-24 w-[40px] sm:w-[60px] md:w-[100px] lg:w-[150px] xl:w-auto'
+                        className='absolute hidden md:block -top-[80px] sm:-top-[100px] md:-top-[150px] lg:-top-[170px] xl:-top-[260px] right-4
+                        sm:right-8 md:right-12 lg:right-16 xl:right-4 w-[40px] sm:w-[40px] md:w-[70px] lg:w-[80px] xl:w-[120px]'
                     />
                 </div>
             </div>
-            <div className='flex flex-col gap-8 sm:gap-12 md:gap-16 lg:gap-20 xl:gap-24 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-0 mt-8 sm:mt-12 md:mt-16 lg:mt-20 xl:mt-24'>
+            <div className='flex flex-col gap-8 sm:gap-12 md:gap-16 lg:gap-20 xl:gap-24 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-0 mt-xw8 sm:mt-12 md:mt-16 lg:mt-20 xl:mt-24'>
                 <List Header='Authentic Job Listings' Text="We source real jobs directly from employer websites, avoiding low-quality listings that seldom lead to interviews." />
                 <List Header='Unlimited Job Sources' Text="Our extensive aggregation spans the internet, ensuring a continuous influx of high-quality job opportunities." />
                 <List Header='Complex Form Handling' Text="Our advanced AI systems adeptly manage intricate application forms, a capability that sets us apart from competitors." />

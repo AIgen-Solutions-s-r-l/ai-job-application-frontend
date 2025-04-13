ef10fb3a9ae5d881bb7f6fa7011967b07f6c53a0
Hussein Hijazi <94968997+sanhijazi@users.noreply.github.com>
2025-04-13 18:33:32 +0200

    Update route.ts

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -23,7 +23,7 @@ export async function GET(req: NextRequest) {
       
       if (result.success) {
         // Successful authentication, redirect to the callback URL
-        return NextResponse.redirect(requestUrl.origin + config.auth.callbackUrl);
+        return NextResponse.redirect('https://pre.laboro.co/' + config.auth.callbackUrl);
       } else {
         // Authentication failed, redirect to login page with error
         const errorMessage = "google_auth_failed";

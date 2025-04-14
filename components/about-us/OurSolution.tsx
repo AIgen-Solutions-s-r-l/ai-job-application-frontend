8ef69e02bbf8ff46a07ab1f51814bdd2c9bc6b96
keat <kavano.aresia@gmail.com>
2025-04-14 16:58:28 +0200

    updated bold letters on about us

--- components/about-us/OurSolution.tsx
+++ components/about-us/OurSolution.tsx
@@ -6,7 +6,8 @@ export const Point = ({ text }: { text: string }) => {
         <div className='flex items-center justify-center gap-4 md:gap-10'>
             <Image src={Flower} alt='Flower' className="w-6 h-6 md:w-auto md:h-auto" />
             <p className="font-montserrat text-black text-base md:text-[20px] font-[400]">
-                {text}
+            <strong>{text.split(':')[0]}:</strong>{' '}
+            {text.split(':').slice(1).join(':')}
             </p>
         </div>
     );
@@ -16,7 +17,10 @@ function OurSolution() {
     return (
         <div className="flex flex-col px-4 sm:px-8 md:px-16 lg:px-24 gap-4 md:gap-8">
             <p className="font-montserrat text-black text-3xl md:text-4xl lg:text-[50px] font-semibold">Our Solution</p>
-            <p className="font-montserrat text-black text-xl md:text-2xl lg:text-[40px] font-[400]">LABORO offers a high-tech platform designed to automate and optimize job applications. Our key features include:</p>
+            <p
+            className="font-montserrat text-black text-xl md:text-2xl lg:text-[40px] font-[400]"
+            style={{ lineHeight: '1.1' }}
+            >LABORO offers a high-tech platform designed to automate and optimize job applications. Our key features include:</p>
             <div className='flex flex-col gap-4 md:gap-8'>
                 <Point text='Comprehensive Job Aggregation: We provide access to the most extensive collection of actual job listings sourced directly from company websites, ensuring authenticity and relevance.' />
                 <Point text='Advanced Application Automation: Our sophisticated AI-powered web agent can navigate and complete even the most complex application forms on your behalf, significantly reducing the time and effort required.' />

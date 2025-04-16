892f8aa3d7657240d0fbca2cbc965ff696c3a6de
DavisuaCoder <davisua.stuff@gmail.com>
2025-04-16 10:04:00 +0200

    refactor: remove Supabase-related files and actions
    - Deleted pricing actions and data files to streamline the codebase.
    - Removed Supabase client and middleware files to eliminate unnecessary complexity.
    - Cleaned up package dependencies by removing Supabase-related packages from package.json and package-lock.json.
    - Removed unused types related to Supabase interactions.

--- components/roles/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getUserJobRoles } from "@/libs/supabase-data";
-import TableActions from "./table";
-
-export default async function TableRolesServerComponent() {
-  // Obtener los roles de trabajo del usuario
-  const jobs = await getUserJobRoles();
-
-  return <TableActions jobs={jobs} initialPageSize={5} />;
-}

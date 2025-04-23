90bf3fbd3b2f9f9f8b8f900cd6b4ff2c6cbf3331
sanhijazi <h.hijazi711@gmail.com>
2025-04-23 13:19:34 +0200

    Progres bar

--- components/onboarding/UploadResumeOnboarding.tsx
+++ components/onboarding/UploadResumeOnboarding.tsx
@@ -10,6 +10,7 @@ import { Container } from "@/components/Container";
 import { AiFillFilePdf } from "react-icons/ai";
 import { ArrowRightIcon } from '@/components/AppIcons';
 import { FaSpinner } from 'react-icons/fa';
+import { useWindowSize } from '@/lib/hooks';
 
 export const UploadResumeOnboarding: React.FC = () => {
   const [cvFile, setCVFile] = useState<File | null>(null);
@@ -18,6 +19,9 @@ export const UploadResumeOnboarding: React.FC = () => {
   const [progress, setProgress] = useState(10);
   const [progressText, setProgressText] = useState('Thinking...');
   const { setCVData } = useCVDataContext();
+
+  const { width } = useWindowSize();
+  const isMobile = width <= 740;
 
   const handleDragOver = (e: DragEvent<HTMLDivElement>) => {
     e.preventDefault();
@@ -96,43 +100,42 @@ export const UploadResumeOnboarding: React.FC = () => {
           onDragOver={handleDragOver}
           onDragLeave={handleDragLeave}
           onDrop={handleDrop}
-          className={`flex flex-col items-center justify-center w-full h-[408px] rounded-[22px] border border-dashed border-my-neutral-7 ${
-            isDragging 
-              ? 'bg-neutral-cold-0' 
-              : cvFile 
-              ? 'animate-gradient' 
+          className={`flex flex-col items-center justify-center w-full h-[408px] rounded-[22px] border border-dashed border-my-neutral-7 ${isDragging
+            ? 'bg-neutral-cold-0'
+            : cvFile
+              ? 'animate-gradient'
               : ''
-          }`}
+            }`}
         >
           {isDragging ? (
             <AiFillFilePdf className='text-7xl text-primary' />
           ) : cvFile ? (
             <>
-              <p className="font-montserrat text-[20px] font-semibold leading-none mb-10">You have successfully uploaded &quot;{cvFile.name}&quot;</p>
+              <p className="font-montserrat text-[20px] font-semibold leading-none mb-10 px-7 text-center">You have successfully uploaded &quot;{cvFile.name}&quot;</p>
               <button
                 type="button"
                 onClick={handleUpload}
-                className="w-[273px] my-btn-green disabled:bg-my-neutral-2 disabled:cursor-not-allowed font-jura font-semibold text-[18px]"
+                className="flex gap-4 sm:gap-24 my-btn-green disabled:bg-my-neutral-2 disabled:cursor-not-allowed font-jura font-semibold text-[18px]"
                 disabled={isLoading}
               >
-                {isLoading 
-                ? (
-                  <>
-                    <p>Creating Resume</p>
-                    <FaSpinner className="animate-spin" />
-                  </>
-                ) : (
-                  <>
-                    <p>Create Resume</p>
-                    <ArrowRightIcon classname="animate-bounce-horizontal" />
-                  </>
-                )}
+                {isLoading
+                  ? (
+                    <>
+                      <p>Creating Resume</p>
+                      <FaSpinner className="animate-spin" />
+                    </>
+                  ) : (
+                    <>
+                      <p>Create Resume</p>
+                      <ArrowRightIcon classname="animate-bounce-horizontal" />
+                    </>
+                  )}
               </button>
             </>
           ) : (
             <>
               <p className="hidden md:inline-block font-montserrat text-[20px] leading-none mb-10">You can also drag & drop your resume here.</p>
-              <label className="w-[273px] my-btn-green cursor-pointer">
+              <label className="flex gap-12 sm:gap-24 my-btn-green cursor-pointer">
                 <p className="font-jura text-[18px] font-semibold">Upload</p>
                 <ArrowRightIcon />
                 <input
@@ -151,12 +154,12 @@ export const UploadResumeOnboarding: React.FC = () => {
         <div className="w-full grow flex items-center justify-center flex-col gap-10">
           <div className="flex gap-4 items-center justify-between text-xl w-[400px]">
           </div>
-          <div className='absolute left-0 bottom-0 h-[28px] w-full text-my-neutral-0' >
+          <div className='fixed left-0 bottom-0 h-[28px] w-full text-my-neutral-0' >
             <div
               className='flex items-center px-4 absolute left-0 h-full bg-primary-light-purple transition-all duration-300'
               style={{ width: `${progress}%` }}
             >
-              <span>{`${progressText} ${progress}%`}</span>
+              <span>{`${!isMobile ? progressText : ''} ${progress}%`}</span>
               <Image
                 src={LaboroSmiley}
                 alt='Smiley Face :)'
